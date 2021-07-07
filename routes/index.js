051435cdca2fa6ed0d2351a586f46c827e245e2a
Noorwahid717 <noorwahid052002@gmail.com>
2021-07-07 19:56:11 +0700

    tambah fitur post profile

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,26 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
+const profiles = {
+  nowah: {
+    name: 'Noor Wahid',
+    company: 'Buroq', 
+    languages: ['javascript', 'python', 'java']
+  },
+
+  sjobs: {
+    name: 'steve jobs',
+    company: 'apple',
+    languages: ['swift', 'c++', 'objective-c']
+  },
+
+  bgates: {
+    name: 'bill gates',
+    company: 'microsoft',
+    languages: ['visual studio', 'microsoft azure']
+  }
+}
+
 router.get('/', (req, res) => {
 
 	res.render('index', {text: 'This is the dynamic data. Open index.js from the routes directory to see.'})
@@ -10,4 +30,44 @@ router.get('/', (req, res) => {
 })
 
 
+router.get('/query', (req, res) => {
+  const name = req.query.name
+  const occupation = req.query.occupation
+
+  const data = {
+    name: name,
+    occupation: occupation
+  }
+
+  res.render('profile', data)
+})
+
+router.get('/:path', (req, res) => {
+  const path = req.params.path
+
+  res.json({
+    data: path
+  })
+})
+
+router.get('/:profile/:username', (req, res) => {
+  const profile = req.params.profile
+  const username = req.params.username
+  const currentProfile = profiles[username]
+
+  if (currentProfile == null){
+    res.json({
+      confirmation: 'gagal',
+      message: 'Profile ' + username + ' Tidak ditemukan'
+    })
+
+    return 
+  }
+
+  res.render('profile', currentProfile)
+  })
+
+
 module.exports = router
+
+// nyoba ngupload project
